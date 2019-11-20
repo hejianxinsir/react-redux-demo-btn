1de81b243f5ab189bba20d08f4aeaef832c2dd6b
hejianxin <hejianxinsir@gmail.com>
2019-11-20 17:52:35 +0800

    react-redux-demo

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+	
+	add1(){
+		this.props.onAdd1()
+	}
+	
+	add2(){
+		this.props.onAdd2()
+	}
+
+	add3(){
+		this.props.onAdd3()
+	}
+
+	add4(){
+		this.props.onAdd4()
+	}
+
+	render(){
+		return (
+			<div>
+				你点击了 <span id="value">{this.props.value}</span> 次
+				<div>
+					<button id="add1" onClick={()=>this.add1()}>+1</button>
+					<button id="add2" onClick={()=>this.add2()}>+2</button>
+					<button id="add3" onClick={()=>this.add3()}>+1</button>
+					<button id="add1After2Sec" onClick={()=>this.add4()}>两秒后+1</button>
+				</div>
+			</div>
+		);
+	}
+	
+}
+
+
+export default App;
